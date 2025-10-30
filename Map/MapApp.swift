9ef2940fd2e828639a6c69357075d8c3c00935bb
Artur Ribeiro <Artur.Carvalho@br.nestle.com>
2025-10-30 19:38:15 -0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Artur Mac on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
